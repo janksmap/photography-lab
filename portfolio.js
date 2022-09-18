90fb4df298124ea8c74aa28bff488e51b904d5e4
Taylor English <taylorjenglish@gmail.com>
2022-09-18 00:17:44 -0600

    Created portfolio page

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+// function gridLayout() {
+//     let grid = document.getElementById('portfolio-grid');
+    
+//     for (let i = 0; i < 49; i++) {
+//         if (i % 3) {
+//             let row = document.createElement('div');
+//             let column = document.createElement('div');
+//             row.classList.add('row');
+//             column.classList.add('col-md');
+//         }
+
+        
+
+//     }
+// }
